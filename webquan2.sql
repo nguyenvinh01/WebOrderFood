b972db628ab94fd27f493da5475d98041308ad9a
nguyenvinh01 <nguyenthanhvinh751@gmail.com>
2022-04-26 14:51:40 +0700

    update category

--- webquan2.sql
+++ webquan2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2022 at 03:22 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.4.20
+-- Generation Time: Apr 26, 2022 at 09:50 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,13 @@ CREATE TABLE `comments` (
   `dateCreated` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`commentID`, `productID`, `userID`, `content`, `dateCreated`) VALUES
+(40, 24, 25, 'fdfg', '2022-04-04');
+
 -- --------------------------------------------------------
 
 --
@@ -55,10 +62,13 @@ CREATE TABLE `tbl_category_product` (
 --
 
 INSERT INTO `tbl_category_product` (`category_id`, `category_name`, `category_desc`, `category_status`, `created_at`, `updated_at`) VALUES
-(0, 'Món ăn nhẹ', 'ccldcb', 4, '2022-03-31 13:29:02', '2022-03-31 13:29:02'),
-(1, 'Món Việt', 'Reprehenderit qui a dolor ratione omnis sunt voluptas laborum vero cupiditate ut vitae vitae', 1, NULL, NULL),
+(1, 'Món Việt', 'Reprehenderit qui a dolor ratione omnis sunt voluptas laborum vero cupiditate ut vitae vitae', 3, NULL, NULL),
 (2, 'Món Thái', 'Eos sit quis distinctio iusto consequatur fugiat temporibus modi earum dignissimos est tempora illum iusto et optio aut possimus.', 2, NULL, NULL),
-(3, 'Món Trung', 'Hic sit asperiores unde est autem perspiciatis non iure at voluptas sint ipsum illum deleniti ratione est esse at.', 3, NULL, NULL);
+(3, 'Món Trung', 'Hic sit asperiores unde est autem perspiciatis non iure at voluptas sint ipsum illum deleniti ratione est esse at.', 3, NULL, NULL),
+(4, 'Đồ ăn nhẹ', 'Đồ ăn nhẹ', 0, NULL, NULL),
+(5, 'Đồ ăn nhẹ', 'Đồ ăn nhẹ', 4, NULL, NULL),
+(6, '123123', '123123', 0, NULL, NULL),
+(7, '123', '22222', 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -95,7 +105,7 @@ CREATE TABLE `tbl_order` (
   `order_id` int(10) NOT NULL,
   `user_id` int(11) NOT NULL DEFAULT 0,
   `shipping_id` int(11) DEFAULT NULL,
-  `order_total` double(8,2) NOT NULL,
+  `order_total` double(12,2) NOT NULL,
   `NguoiNhan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `SDT` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `DiaChi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -108,9 +118,17 @@ CREATE TABLE `tbl_order` (
 --
 
 INSERT INTO `tbl_order` (`order_id`, `user_id`, `shipping_id`, `order_total`, `NguoiNhan`, `SDT`, `DiaChi`, `order_status`, `created_at`) VALUES
-(26, 23, NULL, 90000.00, 'sdasd', '00000000', 'dasadas', 1, '2022-03-30 10:53:13'),
 (27, 23, NULL, 220000.00, 'hung', '1655646655', 'gầm cầu vượt quốc oai', 3, '2022-03-31 21:45:15'),
-(28, 24, NULL, 150000.00, '1', '0365265059', '2', 1, '2022-03-31 21:48:16');
+(28, 24, NULL, 150000.00, '1', '0365265059', '2', 1, '2022-03-31 21:48:16'),
+(29, 1, NULL, 999999.99, 'admin', '0192929292', 'admin', 1, '2022-04-03 22:23:43'),
+(30, 26, NULL, 20000.00, 'vinh', '0978231231', 'ádd', 1, '2022-04-05 14:38:41'),
+(31, 1, NULL, 50000.00, 'admin', '0192929292', '111111', 4, '2022-04-14 21:42:45'),
+(32, 23, NULL, 59900.00, 'hung', '1655646655', 'gầm cầu vượt quốc oai', 3, '2022-04-15 08:35:55'),
+(33, 23, NULL, 999999.99, 'hung', '1655646655', 'gầm cầu vượt quốc oai', 2, '2022-04-15 09:13:28'),
+(34, 23, NULL, 10000000.00, 'hung', '1655646655', 'gầm cầu vượt quốc oai', 1, '2022-04-15 20:06:53'),
+(35, 23, NULL, 5000000.00, 'hung', '1655646655', 'gầm cầu vượt quốc oai', 1, '2022-04-15 20:09:50'),
+(36, 23, NULL, 12564620.00, 'hung', '1655646655', 'gầm cầu vượt quốc oai', 3, '2022-04-15 20:13:57'),
+(37, 23, NULL, 12564620.00, 'hung', '1655646655', 'gầm cầu vượt quốc oai', 3, '2022-04-15 20:24:56');
 
 -- --------------------------------------------------------
 
@@ -122,7 +140,7 @@ CREATE TABLE `tbl_order_details` (
   `order_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `product_num` int(255) NOT NULL,
-  `product_price` double(8,2) NOT NULL
+  `product_price` double(12,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -132,7 +150,21 @@ CREATE TABLE `tbl_order_details` (
 INSERT INTO `tbl_order_details` (`order_id`, `product_id`, `product_num`, `product_price`) VALUES
 (27, 20, 2, 100000.00),
 (27, 16, 1, 20000.00),
-(28, 23, 5, 30000.00);
+(28, 23, 5, 30000.00),
+(29, 22, 1, 999999.99),
+(30, 16, 1, 20000.00),
+(31, 17, 1, 9900.00),
+(31, 18, 1, 50000.00),
+(32, 17, 1, 9900.00),
+(32, 18, 1, 50000.00),
+(33, 17, 2, 9900.00),
+(33, 22, 2, 999999.99),
+(34, 22, 2, 999999.99),
+(35, 22, 1, 999999.99),
+(36, 22, 2, 999999.99),
+(36, 21, 1, 999999.99),
+(37, 22, 2, 5000000.00),
+(37, 21, 1, 2564620.00);
 
 -- --------------------------------------------------------
 
@@ -160,7 +192,7 @@ CREATE TABLE `tbl_product` (
   `product_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `product_desc` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `product_content` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `product_price` double NOT NULL,
+  `product_price` int(12) NOT NULL,
   `product_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `product_status` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -170,17 +202,21 @@ CREATE TABLE `tbl_product` (
 --
 
 INSERT INTO `tbl_product` (`product_id`, `category_id`, `product_name`, `product_desc`, `product_content`, `product_price`, `product_image`, `product_status`) VALUES
-(16, 1, 'Mì tôm', 'mì tôm chua cay', 'chua chua cay cay', 20000, 'https://vinmec-prod.s3.amazonaws.com/images/20201228_032224_820684_mitoms.max-800x800.jpg', 1),
+(16, 1, 'Mì tôm', 'mì tôm chua cay', 'chua chua cay cay', 20000, 'https://vinmec-prod.s3.amazonaws.com/images/20201228_032224_820684_mitoms.max-800x800.jpg', 0),
 (17, 1, 'Bánh mì pate trứng', ' Là quốc hồn ẩm thực Việt Nam', 'đang giảm giá 51%', 9900, 'https://cdn.jamja.vn/blog/wp-content/uploads/2017/09/cach-lam-banh-mi-pate-trung-8.jpg', 1),
 (18, 2, 'Gà Sốt Cay', ' Làm nóng  lòng trái tim chúng ta mỗi khi đông về', 'mua 2 tặng 1', 50000, 'https://songkhoe.medplus.vn/wp-content/uploads/2020/04/cach-lam-ga-sot-cay-ngot-han-quoc.png', 1),
 (19, 0, 'pan cake', 'mềm mại và ngọt ngào', 'mua 3 giảm 25% giá sản phẩm', 700000, 'https://cdn.tgdd.vn/2021/04/CookProductThumb/2-620x620-46.jpg', 1),
 (20, 1, 'Cơm  Gà', 'Khi mua 3 tặng thêm phần gà giá trị 20000đ\r\n', 'Khi mua 3 tặng thêm phần gà\r\nBạn sẽ càng thích thú hơn bởi hương vị mới mẻ, đầy hấp dẫn của nó. Hạt cơm được chiên vàng, tơi xốp, mềm mịn, không quá khô, không quá béo nên khi ăn không bị ngán. Thịt gà được tuyển chọn từ những phần ngon nhất của con gà như đùi, má đùi, góc tư được chiên khéo léo sao cho lớp vỏ ngoài giòn tan, thịt bên trong chắc, ngọt, vẫn còn ứ nước. Ăn kèm với cơm gà là dưa leo, đồ chua, nước tương, tương ớt. Tất cả cùng hòa quyện, tạo nên một hương vị khó cưỡng.', 100000, 'https://i-giadinh.vnecdn.net/2021/01/29/com2-1611892464-7028-1611892596.jpg', 1),
 (21, 2, 'Lẩu Thái', 'Đặc trưng của Lẩu Thái không thể thiếu vị cay của ớt, vị thơm của gừng, sả cùng lá chanh, kết hợp với vị ngọt của nước hầm xương, những nguyên liệu tươi sống của hải sản như cua, mực, tôm, sò, cá,… và đặc biệt không thể thiếu những món rau tươi ăn kèm như nấm, rau muống, rau cải,…Nước lẩu Thái là sự kết hợp của nhiều hương vị nồng nàn và đậm đà: vị chua đặc trưng của lẩu, vị ngọt từ nước hầm và vị cay tinh tế …', 'Đặc trưng của Lẩu Thái không thể thiếu vị cay của ớt, vị thơm của gừng, sả cùng lá chanh, kết hợp với vị ngọt của nước hầm xương, những nguyên liệu tươi sống của hải sản như cua, mực, tôm, sò, cá,… và đặc biệt không thể thiếu những món rau tươi ăn kèm như nấm, rau muống, rau cải,…Nước lẩu Thái là sự kết hợp của nhiều hương vị nồng nàn và đậm đà: vị chua đặc trưng của lẩu, vị ngọt từ nước hầm và vị cay tinh tế …', 2564620, 'https://cdn.nguyenkimmall.com/images/companies/_1/tin-tuc/kinh-nghiem-meo-hay/n%E1%BA%A5u%20%C4%83n/nau-lau-thai-chuan-vi-ngon-nhu-the-nao.jpg', 1),
-(22, 0, ' Gà Kung Pao Tứ Xuyên', 'Ra đời từ tỉnh Tứ Xuyên, gà Kung Pao nổi tiếng với vị cay nồng và thơm béo đặc trưng, do được nấu cùng ớt và đậu phộng. Món ăn này được người dân Trung Quốc chế biến rất công phu. Các nguyên liệu cần có món ăn này gồm có: thịt ức gà; dầu chiên; Đậu phộng rang; ớt đỏ khô; tỏi băm nhỏ; gừng tươi băm nhỏ; hành lá thái nhỏ; nguyên liệu cho gia vị ướp: nước tương, rượu gạo, bột bắp; nguyên liệu cho phần nước sốt: dấm đen Trung Quốc; nước tương; dầu mè; đường trắng; tương đen; bột bắp.', 'Ra đời từ tỉnh Tứ Xuyên, gà Kung Pao nổi tiếng với vị cay nồng và thơm béo đặc trưng, do được nấu cùng ớt và đậu phộng. Món ăn này được người dân Trung Quốc chế biến rất công phu. Các nguyên liệu cần có món ăn này gồm có: thịt ức gà; dầu chiên; Đậu phộng rang; ớt đỏ khô; tỏi băm nhỏ; gừng tươi băm nhỏ; hành lá thái nhỏ; nguyên liệu cho gia vị ướp: nước tương, rượu gạo, bột bắp; nguyên liệu cho phần nước sốt: dấm đen Trung Quốc; nước tương; dầu mè; đường trắng; tương đen; bột bắp.', 5000000, 'https://www.tourtrungquoc.net.vn/images/am-thuc/Ga-Kung-Pao-2.jpg', 1),
+(22, 3, ' Gà Kung Pao Tứ Xuyên', 'Ra đời từ tỉnh Tứ Xuyên, gà Kung Pao nổi tiếng với vị cay nồng và thơm béo đặc trưng, do được nấu cùng ớt và đậu phộng. Món ăn này được người dân Trung Quốc chế biến rất công phu. Các nguyên liệu cần có món ăn này gồm có: thịt ức gà; dầu chiên; Đậu phộng rang; ớt đỏ khô; tỏi băm nhỏ; gừng tươi băm nhỏ; hành lá thái nhỏ; nguyên liệu cho gia vị ướp: nước tương, rượu gạo, bột bắp; nguyên liệu cho phần nước sốt: dấm đen Trung Quốc; nước tương; dầu mè; đường trắng; tương đen; bột bắp.', 'Ra đời từ tỉnh Tứ Xuyên, gà Kung Pao nổi tiếng với vị cay nồng và thơm béo đặc trưng, do được nấu cùng ớt và đậu phộng. Món ăn này được người dân Trung Quốc chế biến rất công phu. Các nguyên liệu cần có món ăn này gồm có: thịt ức gà; dầu chiên; Đậu phộng rang; ớt đỏ khô; tỏi băm nhỏ; gừng tươi băm nhỏ; hành lá thái nhỏ; nguyên liệu cho gia vị ướp: nước tương, rượu gạo, bột bắp; nguyên liệu cho phần nước sốt: dấm đen Trung Quốc; nước tương; dầu mè; đường trắng; tương đen; bột bắp.', 5000000, 'https://www.tourtrungquoc.net.vn/images/am-thuc/Ga-Kung-Pao-2.jpg', 1),
 (23, 0, 'Tiết canh', 'Tiết canh là món ăn tươi sống sử dụng nguyên liệu là tiết động vật tươi được pha với chút nước mắm hoặc nước muối nhạt hãm cho khỏi đông trước khi trộn với những phần thịt, sụn động vật băm nhỏ để làm đông tiết.', 'Tiết canh là món ăn tươi sống sử dụng nguyên liệu là tiết động vật tươi được pha với chút nước mắm hoặc nước muối nhạt hãm cho khỏi đông trước khi trộn với những phần thịt, sụn động vật băm nhỏ để làm đông tiết.', 30000, 'https://cdn.tgdd.vn/2021/10/CookDish/cach-lam-tiet-canh-de-voi-vi-ngon-hap-dan-don-gian-ngay-tai-avt-1200x676.jpg', 1),
-(24, 0, 'Gỏi Cuốn', ' Gỏi cuốn là một món ăn ngon, nổi tiếng của nước ta. Nguyên liệu chế biến món này khá nhiều nhưng cũng đều là những nguyên liệu đơn giản. Gồm thịt băm, trứng gà hoặc trứng vịt, hành tây, hành lá, cà rốt, su hào, giá đỗ, rau mùi, rau thơm, mộc nhĩ, mì hoặc miến,...và một số loại gia vị như mì chính, bột canh, hạt tiêu, nước mắm,...  Và một thứ không thể thiếu để cuốn ngoài đó chính là bánh đa nem.', ' Gỏi cuốn là một món ăn ngon, nổi tiếng của nước ta. Nguyên liệu chế biến món này khá nhiều nhưng cũng đều là những nguyên liệu đơn giản. Gồm thịt băm, trứng gà hoặc trứng vịt, hành tây, hành lá, cà rốt, su hào, giá đỗ, rau mùi, rau thơm, mộc nhĩ, mì hoặc miến,...và một số loại gia vị như mì chính, bột canh, hạt tiêu, nước mắm,...  Và một thứ không thể thiếu để cuốn ngoài đó chính là bánh đa nem.', 50000, 'https://cdn.cet.edu.vn/wp-content/uploads/2018/11/goi-cuon-tom-thit.jpg', 1),
+(24, 0, 'Gỏi Cuốn', ' Gỏi cuốn là một món ăn ngon, nổi tiếng của nước ta. Nguyên liệu chế biến món này khá nhiều nhưng cũng đều là những nguyên liệu đơn giản. Gồm thịt băm, trứng gà hoặc trứng vịt, hành tây, hành lá, cà rốt, su hào, giá đỗ, rau mùi, rau thơm, mộc nhĩ, mì hoặc miến,...và một số loại gia vị như mì chính, bột canh, hạt tiêu, nước mắm,...  Và một thứ không thể thiếu để cuốn ngoài đó chính là bánh đa nem.', ' Gỏi cuốn là một món ăn ngon, nổi tiếng của nước ta. Nguyên liệu chế biến món này khá nhiều nhưng cũng đều là những nguyên liệu đơn giản. Gồm thịt băm, trứng gà hoặc trứng vịt, hành tây, hành lá, cà rốt, su hào, giá đỗ, rau mùi, rau thơm, mộc nhĩ, mì hoặc miến,...và một số loại gia vị như mì chính, bột canh, hạt tiêu, nước mắm,...  Và một thứ không thể thiếu để cuốn ngoài đó chính là bánh đa nem.', 50000, 'https://cdn.cet.edu.vn/wp-content/uploads/2018/11/goi-cuon-tom-thit.jpg', 0),
 (25, 1, 'Phở Gà', 'Phở gà có hương vị riêng ngon không kém phở bò. Quay lại món phở gà vừa nói. Nước dùng bằng xương gà tất nhiên là đứng đầu bảng trong tất cả các loại nước dùng. Nó vừa ngọt, vừa thanh lại điểm những ánh vàng óng ánh của mỡ gà làm cho bát phở thêm ngon mắt.', 'Phở gà có hương vị riêng ngon không kém phở bò. Quay lại món phở gà vừa nói. Nước dùng bằng xương gà tất nhiên là đứng đầu bảng trong tất cả các loại nước dùng. Nó vừa ngọt, vừa thanh lại điểm những ánh vàng óng ánh của mỡ gà làm cho bát phở thêm ngon mắt.', 100000, 'https://trivietphat.net/wp-content/uploads/2021/08/pho-ga1-1.jpg', 1),
-(26, 1, 'Bún Bò', 'Bún bò Huế là một đặc sản của ẩm thực Huế, tùy là bún bò nhưng ngoài thịt bò còn có thịt heo. Hương vị đặc biệt của món ăn này chủ yếu là ở vị cay nồng, mùi sả đặc trưng của nước lèo. Vốn là một hương vị Huế không lẫn vào đâu được và chính điều đó khiến người ăn cứ nhớ mãi về món ăn này.', 'Bún bò Huế là một đặc sản của ẩm thực Huế, tùy là bún bò nhưng ngoài thịt bò còn có thịt heo. Hương vị đặc biệt của món ăn này chủ yếu là ở vị cay nồng, mùi sả đặc trưng của nước lèo. Vốn là một hương vị Huế không lẫn vào đâu được và chính điều đó khiến người ăn cứ nhớ mãi về món ăn này.', 100000, 'https://saigonangi.com/wp-content/uploads/2020/10/5-1-15.png', 1);
+(26, 1, 'Bún Bò', 'Bún bò Huế là một đặc sản của ẩm thực Huế, tùy là bún bò nhưng ngoài thịt bò còn có thịt heo. Hương vị đặc biệt của món ăn này chủ yếu là ở vị cay nồng, mùi sả đặc trưng của nước lèo. Vốn là một hương vị Huế không lẫn vào đâu được và chính điều đó khiến người ăn cứ nhớ mãi về món ăn này.', 'Bún bò Huế là một đặc sản của ẩm thực Huế, tùy là bún bò nhưng ngoài thịt bò còn có thịt heo. Hương vị đặc biệt của món ăn này chủ yếu là ở vị cay nồng, mùi sả đặc trưng của nước lèo. Vốn là một hương vị Huế không lẫn vào đâu được và chính điều đó khiến người ăn cứ nhớ mãi về món ăn này.', 100000, 'https://saigonangi.com/wp-content/uploads/2020/10/5-1-15.png', 1),
+(27, 1, 'Cháo Lòng', 'Những ngày ở nhà mà có một tô cháo lòng  thơm ngon mùi gạo rang đúng điệu, lòng heo mềm béo hấp dẫn thì còn gì bằng.', 'Những ngày ở nhà mà có một tô cháo lòng  thơm ngon mùi gạo rang đúng điệu, lòng heo mềm béo hấp dẫn thì còn gì bằng.', 20000, 'https://cdn.tgdd.vn/2021/09/CookDish/cach-nau-chao-long-mien-tay-don-gian-thom-ngon-kho-cuong-cho-avt-1200x676.jpg', 1),
+(50, 1, 'Giả Cầy', 'Giả Cầy', 'Gải Cầy', 150000, 'https://cdn.tgdd.vn/2021/04/CookProductThumb/2-620x620-46.jpg', 1),
+(51, 0, 'Giả Cầy', 'Giả Cầy', 'Giả Cầy', 150000, 'https://cdn.tgdd.vn/2021/04/CookProductThumb/2-620x620-46.jpg', 1),
+(56, 0, 'Giả Cầy', 'ád', 'ádasd', 150000, 'https://cdn.tgdd.vn/2021/04/CookProductThumb/2-620x620-46.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -267,10 +303,12 @@ CREATE TABLE `tbl_user` (
 --
 
 INSERT INTO `tbl_user` (`user_id`, `name`, `username`, `user_verified_at`, `password`, `role_id`, `user_sex`, `user_tel`, `user_address`, `user_email`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin', NULL, 'admin', 1, 'Nam', '0192929292', 'admin', 'admin@email.com', NULL, NULL, NULL),
+(1, 'admin', 'admin', NULL, 'admin', 1, 'Nam', '0192929292', '111111', 'admin@email.com', NULL, NULL, NULL),
 (2, 'user', 'user', NULL, 'user', 2, 'Nữ', '0192929292', 'a', 'user@email.com', NULL, NULL, NULL),
 (23, 'hung', 'user1', NULL, '123456', 2, 'Nam', '1655646655', 'gầm cầu vượt quốc oai', 'hung@gmail.com', NULL, NULL, NULL),
-(24, '', 'user2', NULL, '123456', 2, NULL, NULL, NULL, 'hung@gmail.com', NULL, NULL, NULL);
+(24, '', 'user2', NULL, '123456', 2, NULL, NULL, NULL, 'hung@gmail.com', NULL, NULL, NULL),
+(25, 'Nguyen A', 'admin1', NULL, '111111', 2, 'Nam', '0978231231', 'hanoi', 'sdfsdsd@gmail.com', NULL, NULL, NULL),
+(26, 'Vinh', 'admin2', NULL, '111111', 2, 'Nam', '0978231231', 'HN', 'sdfsdsd@gmail.com', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -343,7 +381,13 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `commentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `commentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `tbl_category_product`
+--
+ALTER TABLE `tbl_category_product`
+  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tbl_images`
@@ -355,13 +399,13 @@ ALTER TABLE `tbl_images`
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `tbl_product`
 --
 ALTER TABLE `tbl_product`
-  MODIFY `product_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `product_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `tbl_subimageproduct`
@@ -373,7 +417,7 @@ ALTER TABLE `tbl_subimageproduct`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Constraints for dumped tables
