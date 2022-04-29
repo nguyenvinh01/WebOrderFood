6c5279e6bd9cfda8c139c2fdf895c4eb579f6f12
hung202 <artaimer00@gmail.com>
2022-04-29 22:35:46 +0700

    2

--- admin/MVC/Models/customer.php
+++ admin/MVC/Models/customer.php
@@ -21,51 +21,50 @@ class customer extends Model
        }
       
        
-       function update_account($data1,$data2,$data3,$data4,$data5,$data6,$data7)
-       {   
-          
-            $query = "UPDATE tbl_user SET  name='$data1', user_sex='$data2', user_tel='$data3',user_email='$data4',
-            user_address='$data5',role_id='$data6', user_id='$data7'
-
-            WHERE  user_id =$id; ";
-         $result = $this->conn->query($query);
+      // function update_account($data1,$data2,$data3,$data4,$data5)
+      //  {   
+       //    $id= intval($_SESSION['login']['user_id']);
+       //     $query = "UPDATE tbl_user SET  name='$data1', user_sex='$data2', user_tel='$data3',user_email='$data4',user_address='$data5'  
+      //      WHERE  user_id =$id; ";
+    
+      //    $result = $this->conn->query($query);
             
-            if ($result == true) {
-               setcookie('doimk', 'Cập nhật tài khoản thành công', time() + 2);
-               header("location: ?act=home");
+     //       if ($result == true) {
+     //           setcookie('doimk', 'Cập nhật tài khoản thành công', time() + 2);
+      //         header("location: ?act=home");
+      //     } else {
+     //          setcookie('doimk', 'Mật khẩu xác nhận không đúng', time() + 2);
+     //          header("location: ?act=info");
+     //      }
+    //   }
+
+
+     function update($id,$data)
+      {
+         $v = "";
+         foreach ($data as $key => $value) {
+              $v .= $key . "='" . $value . "',";
+          }
+         $v = trim($v, ",");
+  
+  
+          $query = "UPDATE $this->table SET  $v   WHERE $this->contens = $id";
+          
+  
+          $result = $this->conn->query($query);
+          
+          if ($result == true) {
+              header("location: ?act=controller&xuli=join");
           } else {
-               setcookie('doimk', 'Mật khẩu xác nhận không đúng', time() + 2);
-               header("location: ?act=info");
-           }
-       }
-
-
-     //function update($id,$data)
-     // {
-       //  $v = "";
-       //  foreach ($data as $key => $value) {
-       //       $v .= $key . "='" . $value . "',";
-       //   }
-       //  $v = trim($v, ",");
-  
-  
-       //  $query = "UPDATE $this->table SET  $v   WHERE $this->contens = $id";
-          
-  
-       // $result = $this->conn->query($query);
-          
-       ///   if ($result == true) {
-       //       header("location: ?act=controller&xuli=join");
-        //  } else {
-        //      setcookie('msg', 'Update vào không thành công', time() + 2);
-        //    header("location: ?act=controller&xuli=update");
-        //  }
-     // }
-      //function find($id)
-      //{
-      //    $query = "select * from $this->table where $this->contens =$id";
-      //    return $this->conn->query($query)->fetch_assoc();
-     // }
+              setcookie('msg', 'Update vào không thành công', time() + 2);
+            header("location: ?act=controller&xuli=update");
+          }
+      }
+      function find($id)
+      {
+          $query = "select * from $this->table where $this->contens =$id";
+          return $this->conn->query($query)->fetch_assoc();
+      }
    
 } 
 
